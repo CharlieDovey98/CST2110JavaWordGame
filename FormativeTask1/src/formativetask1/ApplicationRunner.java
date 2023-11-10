270c482a82bfa317c1f23498f021b4996eee5a95
Charlie Dovey <CharlieDovey98@hotmail.co.uk>
2023-11-10 22:39:26 +0000

    Removed .gitignore

--- FormativeTask1/src/formativetask1/ApplicationRunner.java
+++ FormativeTask1/src/formativetask1/ApplicationRunner.java
@@ -5,12 +5,6 @@ import java.io.File;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        
-        //FilePathing Information
-        String dataFile = System.getProperty("user.dir") + File.separator + "datafile.txt";
-        String fileLocation = System.getProperty("user.dir");
-        String dataPath = fileLocation + File.separator + "datafile.txt";
-        //System.out.println("dataFile" + dataFile + "\nfileLocation" + fileLocation + "\ndataPath" + dataPath);
 
         WordGame wordGame = new WordGame();
         wordGame.start();
--- FormativeTask1/src/formativetask1/WordGame.java
+++ FormativeTask1/src/formativetask1/WordGame.java
@@ -1,6 +1,7 @@
 package formativetask1;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -9,11 +10,26 @@ public class WordGame {
 
     static boolean playerOneInitalWord = false;
     static boolean playerTwoInitalWord = false;
-    static String playerOneInitialwordLastChar = "";
+    char playerOneInitialwordLastChar = 'a';
+    StringBuilder scoreBoard = new StringBuilder(
+            "------------------------------------------------------------\n"
+            + "| word | word total | running total |\n");
+    
+           /* + "------------------------------------------------------------\n"
+            + "| ace (1 + 3 + 5) | 9 | 9 |\n"
+            + "------------------------------------------------------------\n"
+            + "| end (5 + 14 + 4) | 23 | 32 |\n"
+            + "------------------------------------------------------------");*/
+    
+//FilePathing Information
+    static String dataFile = System.getProperty("user.dir") + File.separator + "datafile.txt";
+    //System.out.println("dataFile " + dataFile);
 
     public void start() {
+
         GameManagement gameManager = new GameManagement();
         Scanner sc = new Scanner(System.in);
+
         System.out.println("This is a two player game,"
                 + "\nplayers will take it in turns to input a 3 letter word and a score will be calculated from that word. first to above 200 points loses!"
                 + "\nKey: A = 1 point, B = 2 points... Z = 26 points"
@@ -37,6 +53,9 @@ public class WordGame {
                     gameManager.addToPlayerOneScore(value(firstWord));
 
                     //print expression, string builder, eg. cat (3+1+20), word total, running total
+                    
+                    // set the char playerOneInitialwordLastChar to the last index of player ones word for use in player twos turn
+                    playerOneInitialwordLastChar = (firstWord.charAt(2));
                     // change boolean expression playerOneInitalWord to true so their next words dont have to be < 20
                     playerOneInitalWord = true;
                     System.out.println("Player 2 to choose their first word ...");
@@ -45,11 +64,11 @@ public class WordGame {
                 }
             } else if (playerTwoInitalWord == false) {
 
-                System.out.println("Enter a 3-letter word, which must have a value of 20 or less, and must start with " /*+ lastletter*/ + " or enter * to give up > ");// need to have a give up option via *
+                System.out.println("Enter a 3-letter word, which must have a value of 20 or less, and must start with the letter " + playerOneInitialwordLastChar + "\nor enter * to give up > ");// need to have a give up option via *
                 String firstInputWord = sc.next();
                 String firstWord = firstInputWord.toLowerCase();
 
-                if (StartingWordIsValid(firstWord)) {
+                if (StartingWordIsValid(firstWord) && startingWordCharacter(firstWord, playerOneInitialwordLastChar)) {
                     System.out.println("word exists and is lower than 20");
                     //pass word to value method to assertain the value
                     System.out.println("the value of your word is: " + value(firstWord)); // string builder here
@@ -68,7 +87,7 @@ public class WordGame {
                 String inputword = sc.next();
                 String word = inputword.toLowerCase();
 
-                if ((containsString("datafile.txt", word)) && (isValid(word))) {
+                if ((containsString(dataFile, word)) && (isValid(word))) {
                     System.out.println("word exists");
                     //pass word to value method to assertain the value
                     //add value to the players score
@@ -88,9 +107,9 @@ public class WordGame {
     }
 
     // This method checks if the word exists in the datafile
-    public boolean containsString(String fileName, String word) {
+    public boolean containsString(String dataFile, String word) {
         try {
-            BufferedReader buff = new BufferedReader(new FileReader(fileName));
+            BufferedReader buff = new BufferedReader(new FileReader(dataFile));
             String s;
             while ((s = buff.readLine()) != null) {
                 if (s.trim().contains(word)) {
@@ -120,7 +139,7 @@ public class WordGame {
         // player ones word must be lower than 20, and be a valid word within datafile.txt
         // set game start to true
 
-        return (isValid(firstWord) && ((value(firstWord)) <= 20) && containsString("datafile.txt", firstWord));
+        return (isValid(firstWord) && ((value(firstWord)) <= 20) && containsString(dataFile, firstWord));
     }
 
     public int value(String word) {
@@ -133,5 +152,10 @@ public class WordGame {
         }
 
         return sum;
+    }
+
+    public boolean startingWordCharacter(String firstWord, char playerOneInitialwordLastChar) {
+        return (playerOneInitialwordLastChar == firstWord.charAt(0));
+
     }
 }
