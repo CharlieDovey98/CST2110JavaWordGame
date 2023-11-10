8b8eec2c8c278966a80fe48d07040727c95fee5e
Charlie Dovey <CharlieDovey98@hotmail.co.uk>
2023-11-10 19:19:20 +0000

    Adding base files for assessment task1

---
+++ FormativeTask1/src/formativetask1/ApplicationRunner.java
@@ -0,0 +1,16 @@
+
+package formativetask1;
+
+import java.io.File;
+
+
+public class ApplicationRunner {
+
+
+    public static void main(String[] args) {
+        
+        String dataFile = System.getProperty("user.dir") + File.separator + "datafile.txt";
+        
+    }
+    
+}
